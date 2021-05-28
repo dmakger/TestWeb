395aa61f2d4ec24780498053f2d3c68c2bf8948d
Keeper-of-honor <keepka18@gmail.com>
2021-05-29 00:16:15 +0300

    Добавил сайт fotoprint

---
+++ foto_print/printing/downoload.js
@@ -0,0 +1,23 @@
+// if (window.innerWidth < 430) {
+// 	//let arr_links = document.getElementById("links");
+// 	let arr_links = document.getElementsByClassName('rectangle');
+// 	arr_links.setAttribute('a', download);
+// 	console.log(arr_links);
+// }
+
+/*
+var bruh = document.getElementsByClassName('block2')
+console.log(bruh);
+var blocksFild2 = document.getElementsByClassName('block2__content');
+var content__boxFild = document.getElementsByClassName('content__box');
+console.log(blocksFild2[0]);
+console.log(blocksFild2[1]);
+*/
+console.log(document.getElementById('links'));
+if (window.innerWidth < 430) {
+	document.getElementById('links').style.display = "none";
+	document.getElementById('downloads').style.display = "block";
+} else{
+	document.getElementById('links').style.display = "block";
+	document.getElementById('downloads').style.display = "none";
+}
